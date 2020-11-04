aa56dc9904dcec9cf15ee14d13bd15859632dc5d
Subramonian Inian <risubramonian@gmail.com>
2020-11-04 17:44:05 +0530

    A windows application to trigger an Azure data factory pipeline

---
+++ ADF_Analyzer/ADFAnlayzer.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace ADF_Analyzer
+{
+    partial class ADFAnlayzer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ADFAnlayzer));
+            this.resourceGrp = new System.Windows.Forms.TextBox();
+            this.appID = new System.Windows.Forms.TextBox();
+            this.tenant = new System.Windows.Forms.TextBox();
+            this.authKey = new System.Windows.Forms.TextBox();
+            this.dsName = new System.Windows.Forms.TextBox();
+            this.plName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.plParam = new System.Windows.Forms.TextBox();
+            this.outputDetails = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.subId = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resourceGrp
+            // 
+            this.resourceGrp.Location = new System.Drawing.Point(139, 109);
+            this.resourceGrp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.resourceGrp.Name = "resourceGrp";
+            this.resourceGrp.Size = new System.Drawing.Size(241, 24);
+            this.resourceGrp.TabIndex = 1;
+            // 
+            // appID
+            // 
+            this.appID.Location = new System.Drawing.Point(139, 147);
+            this.appID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.appID.Name = "appID";
+            this.appID.Size = new System.Drawing.Size(241, 24);
+            this.appID.TabIndex = 2;
+            // 
+            // tenant
+            // 
+            this.tenant.Location = new System.Drawing.Point(139, 185);
+            this.tenant.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tenant.Name = "tenant";
+            this.tenant.Size = new System.Drawing.Size(241, 24);
+            this.tenant.TabIndex = 3;
+            // 
+            // authKey
+            // 
+            this.authKey.Location = new System.Drawing.Point(139, 223);
+            this.authKey.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.authKey.Name = "authKey";
+            this.authKey.Size = new System.Drawing.Size(241, 24);
+            this.authKey.TabIndex = 4;
+            // 
+            // dsName
+            // 
+            this.dsName.Location = new System.Drawing.Point(139, 261);
+            this.dsName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dsName.Name = "dsName";
+            this.dsName.Size = new System.Drawing.Size(241, 24);
+            this.dsName.TabIndex = 5;
+            // 
+            // plName
+            // 
+            this.plName.Location = new System.Drawing.Point(139, 299);
+            this.plName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.plName.Name = "plName";
+            this.plName.Size = new System.Drawing.Size(241, 24);
+            this.plName.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Subscription ID : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Resource Group :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Application ID :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tenant ID :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 19);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Authentication Key :";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Data Factory Name :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1, 299);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 19);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "PipeLine Name : ";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(1, 341);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 19);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Pipeline Parameters :";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // plParam
+            // 
+            this.plParam.Location = new System.Drawing.Point(139, 337);
+            this.plParam.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.plParam.Multiline = true;
+            this.plParam.Name = "plParam";
+            this.plParam.Size = new System.Drawing.Size(418, 124);
+            this.plParam.TabIndex = 14;
+            // 
+            // outputDetails
+            // 
+            this.outputDetails.Location = new System.Drawing.Point(5, 496);
+            this.outputDetails.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.outputDetails.Multiline = true;
+            this.outputDetails.Name = "outputDetails";
+            this.outputDetails.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputDetails.Size = new System.Drawing.Size(674, 143);
+            this.outputDetails.TabIndex = 16;
+            this.outputDetails.TextChanged += new System.EventHandler(this.outputDetails_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(701, 557);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 34);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Trigger Pipeline";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.TriggerPipeline);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(3, 473);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 19);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Output Window :";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Sitka Heading", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Firebrick;
+            this.label10.Location = new System.Drawing.Point(49, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(245, 53);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "ADF Analyzer";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // subId
+            // 
+            this.subId.Location = new System.Drawing.Point(139, 66);
+            this.subId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.subId.Name = "subId";
+            this.subId.Size = new System.Drawing.Size(241, 24);
+            this.subId.TabIndex = 20;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(701, 491);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 34);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Ping";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ping);
+            // 
+            // ADFAnlayzer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(828, 652);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.subId);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputDetails);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.plParam);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plName);
+            this.Controls.Add(this.dsName);
+            this.Controls.Add(this.authKey);
+            this.Controls.Add(this.tenant);
+            this.Controls.Add(this.appID);
+            this.Controls.Add(this.resourceGrp);
+            this.Font = new System.Drawing.Font("Sitka Heading", 9.749999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ADFAnlayzer";
+            this.Text = "ADF Analyzer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox resourceGrp;
+        private System.Windows.Forms.TextBox appID;
+        private System.Windows.Forms.TextBox tenant;
+        private System.Windows.Forms.TextBox authKey;
+        private System.Windows.Forms.TextBox dsName;
+        private System.Windows.Forms.TextBox plName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox plParam;
+        private System.Windows.Forms.TextBox outputDetails;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox subId;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ADF_Analyzer/ADFAnlayzer.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.Rest;
+using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Text.RegularExpressions;
+
+namespace ADF_Analyzer
+{
+    public partial class ADFAnlayzer : Form
+    {
+
+        public ADFAnlayzer()
+        {
+            InitializeComponent();
+        }
+        static DataFactoryManagementClient client;
+
+        private void TriggerPipeline(object sender, EventArgs e)
+        {
+            string tenantID = tenant.Text;
+            string applicationId = appID.Text;
+            string authenticationKey = authKey.Text;
+            string subscriptionId = subId.Text;
+            string resourceGroup = resourceGrp.Text;
+            string dataFactoryName = dsName.Text;
+            string pipelineName = plName.Text;
+            string pipelineParameters = Regex.Replace(plParam.Text, @"\s+", string.Empty);
+
+            // Authorize Azure
+            try
+            {
+                outputDetails.Text = string.Empty;
+                outputDetails.Text = "Initiating Azure Authorization...";
+                AuthenticationContext context = new AuthenticationContext("https://login.windows.net/" + tenantID);
+                ClientCredential cc = new ClientCredential(applicationId, authenticationKey);
+                AuthenticationResult result = context.AcquireTokenAsync("https://management.azure.com/", cc).Result;
+                ServiceClientCredentials cred = new TokenCredentials(result.AccessToken);
+
+                client = new DataFactoryManagementClient(cred)
+                {
+                    SubscriptionId = subscriptionId
+                };
+
+                outputDetails.Text = outputDetails.Text + "\r\nAzure Authorization Complete...";
+            }
+            catch (Exception ex)
+            {
+                outputDetails.Text = outputDetails.Text + "\r\n Azure Authorization failed..." + "\r\n" + ex.InnerException;
+            }
+
+            // Set parameters {parameter_name, value}
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            string[] items = pipelineParameters.TrimEnd(';').Split(';');
+            foreach (string item in items)
+            {
+                string[] keyValue = item.Split('=');
+                parameters.Add(keyValue[0], keyValue[1]);
+            }
+
+            // Trigger Azure Datafactory Pipeline
+            try
+            {
+                outputDetails.Text = outputDetails.Text + "\r\nInitiating pipeline run...";
+                CreateRunResponse runResponse = client.Pipelines.CreateRunWithHttpMessagesAsync(
+                    resourceGroup,
+                    dataFactoryName,
+                    pipelineName,
+                    parameters: parameters
+                ).Result.Body;
+
+                outputDetails.Text = outputDetails.Text + "\r\nPipeline run ID: " + runResponse.RunId;
+
+                outputDetails.Text = outputDetails.Text + "\r\nChecking pipeline run status...";
+                PipelineRun pipelineRun;
+                while (true)
+                {
+                    pipelineRun = client.PipelineRuns.Get(
+                        resourceGroup, dataFactoryName, runResponse.RunId);
+                    outputDetails.Text = outputDetails.Text + "\r\nStatus: " + pipelineRun.Status;
+                    if (pipelineRun.Status == "InProgress" || pipelineRun.Status == "Queued")
+                        System.Threading.Thread.Sleep(15000);
+                    else
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                outputDetails.Text = "Pipeline Trigger failed..." + "\r\n" + ex.InnerException; ;
+            }
+
+        }
+
+        private void ping(object sender, EventArgs e)
+        {
+            try
+            {
+                string tenantID = tenant.Text;
+                string applicationId = appID.Text;
+                string authenticationKey = authKey.Text;
+                string subscriptionId = subId.Text;
+
+                outputDetails.Text = string.Empty;
+                outputDetails.Text = "Initiating Azure Authorization...";
+                AuthenticationContext context = new AuthenticationContext("https://login.windows.net/" + tenantID);
+                ClientCredential cc = new ClientCredential(applicationId, authenticationKey);
+                AuthenticationResult result = context.AcquireTokenAsync("https://management.azure.com/", cc).Result;
+                ServiceClientCredentials cred = new TokenCredentials(result.AccessToken);
+
+                client = new DataFactoryManagementClient(cred)
+                {
+                    SubscriptionId = subscriptionId
+                };
+
+                outputDetails.Text = outputDetails.Text + "\r\n Azure Authorization Successful...!";
+            }
+            catch (Exception ex)
+            {
+                outputDetails.Text = outputDetails.Text + "\r\n Ping Failed...!" + "\r\n" + ex.InnerException;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void subscriptionId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputDetails_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
